5b5d2e97d2dedb244f8b7c0f1c10234d785c897d
gaspardarosafrancisco@gmail.com <gaspar-da-rosa.francisco@etu.univ-usmb.fr>
2024-06-27 14:27:54 +0200

    SET: defined settings

--- book-social/src/main/java/com/gaspar/book/BookSocialApplication.java
+++ book-social/src/main/java/com/gaspar/book/BookSocialApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookSocialNetworkApplication {
+public class BookSocialApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookSocialApplication.class, args);
---
+++ book-social/src/main/java/com/gaspar/book/User.java
@@ -0,0 +1,6 @@
+package com.gaspar.book;
+
+
+public User implements UserDetails, Principal{
+
+}
