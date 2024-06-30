df83dc78635fd4afac9e6daa4a312fa5edddb443
gaspardarosafrancisco@gmail.com <gaspar-da-rosa.francisco@etu.univ-usmb.fr>
2024-06-30 16:41:52 +0200

    CREATE-Service: UserDetailService

---
+++ book-social/src/main/java/com/gaspar/book/security/UserDetailService.java
@@ -0,0 +1,23 @@
+package com.gaspar.book.security;
+
+import com.gaspar.book.user.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional //to have the authorities with the user details
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return userRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
--- book-social/src/main/java/com/gaspar/book/security/UserDetailsService.java
+++
@@ -1,4 +0,0 @@
-package com.gaspar.book.security;
-
-public class UserDetailsService {
-}
