d0f29af272d9960eaf15b879036853801be09cdf
gaspardarosafrancisco@gmail.com <gaspar-da-rosa.francisco@etu.univ-usmb.fr>
2024-06-30 11:37:42 +0200

    CREATE: Role class with properties

--- book-social/src/main/java/com/gaspar/book/role/Role.java
+++ book-social/src/main/java/com/gaspar/book/role/Role.java
@@ -1,4 +1,39 @@
 package com.gaspar.book.role;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.gaspar.book.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Role {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @Column(unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    @JsonIgnore //don't show all the users with this role at serialization
+    private List<User> users;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdDate;
+    @LastModifiedDate
+    @Column(insertable = false) //no need value in the creation
+    private LocalDateTime lastModifiedDate;
 }
--- book-social/src/main/java/com/gaspar/book/user/User.java
+++ book-social/src/main/java/com/gaspar/book/user/User.java
@@ -1,11 +1,13 @@
 package com.gaspar.book.user;
 
+import com.gaspar.book.role.Role;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.security.Principal;
@@ -13,6 +15,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -22,8 +25,7 @@ import java.util.List;
 @AllArgsConstructor
 @Table(name = "_user")
 @EntityListeners(AuditingEntityListener.class)
-
-class User implements UserDetails, Principal{
+public class User implements UserDetails, Principal{
 
     @Id
     @GeneratedValue
@@ -36,7 +38,10 @@ class User implements UserDetails, Principal{
     private String password;
     private boolean accountLocked;
     private boolean enabled;
-    //private List<Role> roles;
+
+    //when fetching the user, fetch the routes
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<Role> roles;
 
     @CreatedDate
     @Column(nullable = false, updatable = false)
@@ -52,7 +57,10 @@ class User implements UserDetails, Principal{
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
     }
 
     @Override
