7e81ef52c629a4be06079da785630cc1308dcaad
gaspardarosafrancisco@gmail.com <gaspar-da-rosa.francisco@etu.univ-usmb.fr>
2024-07-10 21:04:21 +0200

    Book: find book by id

--- book-social/src/main/java/com/gaspar/book/book/BookService.java
+++ book-social/src/main/java/com/gaspar/book/book/BookService.java
@@ -1,6 +1,7 @@
 package com.gaspar.book.book;
 
 import com.gaspar.book.user.User;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
@@ -20,5 +21,8 @@ public class BookService {
     }
 
     public BookResponse findById(Integer bookId) {
+        return bookRepository.findById(bookId)
+                .map(bookMapper::toBookResponse)
+                .orElseThrow(() -> new EntityNotFoundException("No book found with the ID::" + bookId));
     }
 }
