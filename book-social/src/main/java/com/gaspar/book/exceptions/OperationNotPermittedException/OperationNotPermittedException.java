aeba799f4f535d4ff93739f7b79892581f4e9056
gaspardarosafrancisco@gmail.com <gaspar-da-rosa.francisco@etu.univ-usmb.fr>
2024-07-12 13:44:49 +0200

    Query: update shareable status.

---
+++ book-social/src/main/java/com/gaspar/book/exceptions/OperationNotPermittedException/OperationNotPermittedException.java
@@ -0,0 +1,6 @@
+package com.gaspar.book.exceptions.OperationNotPermittedException;
+
+public class OperationNotPermittedException extends Throwable {
+    public OperationNotPermittedException(String s) {
+    }
+}
