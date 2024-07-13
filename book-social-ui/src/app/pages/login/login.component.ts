feae4848f367452abb897261e2934cc8b0566523
gaspardarosafrancisco@gmail.com <gaspar-da-rosa.francisco@etu.univ-usmb.fr>
2024-07-13 19:01:56 +0200

    feat(login): login implementation part 1

---
+++ book-social-ui/src/app/pages/login/login.component.ts
@@ -0,0 +1,43 @@
+import {Component, NgModule} from '@angular/core';
+import {AuthenticationRequest} from "../../services/models/authentication-request";
+import {NgForOf, NgIf} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+import {Router} from "@angular/router";
+import {AuthenticationService} from "../../services/services/authentication.service";
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    NgForOf,
+    NgIf,
+    FormsModule
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+  authRequest: AuthenticationRequest = {email:'', password: ''};
+  errorMsg: Array<String> = [];
+
+  constructor(private router: Router, private authService: AuthenticationService) {
+  }
+
+  login() {
+    this.errorMsg = [];
+    this.authService.authenticate({
+      body: this.authRequest
+    }).subscribe({
+      next: value => {
+        //save token
+        this.router.navigate(['api/v1/books']);
+      },
+      error: err => {console.log(err)}
+    })
+  }
+
+  goToRegister() {
+    this.router.navigate(['api/v1/auth/register']).then(r => console.log(r));
+  }
+}
