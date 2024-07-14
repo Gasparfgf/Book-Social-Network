5de565a062621ed71aefe35d5ab2644855ba7447
gaspardarosafrancisco@gmail.com <gaspar-da-rosa.francisco@etu.univ-usmb.fr>
2024-07-14 23:44:55 +0200

    chore(activate-account): changes to the authentication implementation

--- book-social-ui/src/app/pages/activate-account/activate-account.component.ts
+++ book-social-ui/src/app/pages/activate-account/activate-account.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
+import {AuthenticationService} from "../../services/services/authentication.service";
+import {CodeInputModule} from "angular-code-input";
 
 @Component({
   selector: 'app-activate-account',
   standalone: true,
-  imports: [],
+  imports: [
+    CodeInputModule
+  ],
   templateUrl: './activate-account.component.html',
   styleUrl: './activate-account.component.scss'
 })
 export class ActivateAccountComponent {
 
+  message: string = '';
+  isOk: boolean = true;
+  submitted: boolean = false;
+
+  constructor(private router: Router, authService: AuthenticationService) {
+  }
+
+  oncodeCompleted(activationToken: string) {
+
+
+  }
 }
