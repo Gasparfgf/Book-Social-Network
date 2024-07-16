4b750254e9515a5e697cae78a64965f9fa2469b4
gaspardarosafrancisco@gmail.com <gaspar-da-rosa.francisco@etu.univ-usmb.fr>
2024-07-16 12:34:33 +0200

    feat(menu): implement menu

--- book-social-ui/src/app/modules/book/components/menu/menu.component.ts
+++ book-social-ui/src/app/modules/book/components/menu/menu.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit{
 
+  logout() {
+
+  }
+
+  ngOnInit(): void {
+    const linkcolor = document.querySelectorAll('.nav-link');
+    linkcolor.forEach(link => {
+      if(window.location.href.endsWith(link.getAttribute('href') || '')){
+        link.classList.add('active');
+      }
+      link.addEventListener('click', () => {
+        linkcolor.forEach(lnk => lnk.classList.remove('active'));
+        link.classList.add('active');
+      })
+    })
+  }
 }
